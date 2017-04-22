83439cdb9c302ad934d4735760d94ed6409d7dc1
Wei <iqboss@gmail.com>
2017-04-22 01:03:22 -0400

    Unit test for DayCareCenter

--- grails-app/domain/daycare/provider/DayCareCenter.groovy
+++ grails-app/domain/daycare/provider/DayCareCenter.groovy
@@ -1,24 +1,25 @@
 package daycare.provider
 
 class DayCareCenter {
-    Integer centerCapcity
-    BigDecimal dailyRate
     String name
     String address
     String city
     String state
     String zip
     String email
-    String  phoneNumber
+    String phoneNumber
     String otherDetail
+    Integer centerCapcity
+    BigDecimal dailyRate
 
     static hasMany = [features:Feature, pictures:Picture,calendars:Calendar]
 
     static constraints = {
         email nullable: true
-        phoneNumber nullable: true
-        phoneNumber(matches: '^\\d{3}-\\d{3}-\\d{4}$')
-        dailyRate nullable: true
+        phoneNumber nullable: true, matches: '^\\d{3}-\\d{3}-\\d{4}$'
+        otherDetail nullable: true
+        centerCapcity min: 1
+        dailyRate min: 0.0
         state inList: ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI',
                             'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN',
                             'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY',
--- src/test/groovy/daycare/provider/DayCareCenterSpec.groovy
+++ src/test/groovy/daycare/provider/DayCareCenterSpec.groovy
@@ -15,8 +15,72 @@ class DayCareCenterSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test validation of nullable objects"() {
+        when:
+        DayCareCenter D1 = new DayCareCenter()
+        then:
+        !D1.validate()
+
+        when:
+        D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'MA',
+                                                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 6,dailyRate: 25.45)
+        then:
+        D1.validate()
+    }
+
+    void "test phone format"() {
+        when:
+        DayCareCenter D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'MA',
+                zip: '02135',phoneNumber: '1-123-456-7890',centerCapcity: 6,dailyRate: 25.45)
+        then:
+        !D1.validate()
+
+        when:
+        D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'MA',
+                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 6,dailyRate: 25.45)
+        then:
+        D1.validate()
+    }
+
+    void "test state inList"() {
+        when:
+        DayCareCenter D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'XX',
+                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 6,dailyRate: 25.45)
+        then:
+        !D1.validate()
+
+        when:
+        D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'MA',
+                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 6,dailyRate: 25.45)
+        then:
+        D1.validate()
+    }
+
+    void "test centerCapcity"() {
+        when:
+        DayCareCenter D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'XX',
+                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 0,dailyRate: 25.45)
+        then:
+        !D1.validate()
+
+        when:
+        D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'MA',
+                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 6,dailyRate: 25.45)
+        then:
+        D1.validate()
+    }
+
+    void "test dailyRate"() {
+        when:
+        DayCareCenter D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'XX',
+                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 6,dailyRate: -25.45)
+        then:
+        !D1.validate()
+
+        when:
+        D1 = new DayCareCenter(name: 'My Day Care',address: '123 Main st',city: 'Boston',state: 'MA',
+                zip: '02135',phoneNumber: '123-456-7890',centerCapcity: 6,dailyRate: 25.45)
+        then:
+        D1.validate()
     }
 }
