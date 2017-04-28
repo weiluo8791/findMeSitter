c00b29c6716fc375cbf4dd909e08e96323276dc1
Wei <iqboss@gmail.com>
2017-04-28 15:25:06 -0400

    added userdetail plugin setup and change Reviewer domain to use the plugin

--- grails-app/domain/user/review/Reviewer.groovy
+++ grails-app/domain/user/review/Reviewer.groovy
@@ -1,15 +1,12 @@
 package user.review
 
+import findMeSitter.user.*
+
 class Reviewer {
-    String firstName
-    String lastName
-    String nickName
-    String gender
-    String city
-    String state
-    String otherDetail
     Date dateOfFirstReview
     Date dateOfLatestReview
+
+    User userDetail
 
     Integer getTotalReviewCount() {
         this.reviews.size()
@@ -17,17 +14,7 @@ class Reviewer {
 
     static transients = ['totalReviewCount']
     static hasMany = [reviews: Review]
-
     static constraints = {
-        nickName nullable: true
-        gender nullable: true, inList: ['M', 'F']
-        city nullable: true
-        state nullable: true, inList: ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI',
-                                       'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN',
-                                       'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY',
-                                       'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT',
-                                       'WA', 'WI', 'WV', 'WY']
-        otherDetail nullable: true
         dateOfFirstReview nullable: true
         dateOfLatestReview nullable: true, validator: { val, obj ->
             val?.after(obj.dateOfFirstReview) || (val == obj.dateOfFirstReview)
